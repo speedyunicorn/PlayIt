0e08086f444c894c5bb79591ab655bd645d2c3ab
speedyunicorn <speedyunicorn777@gmail.com>
2015-08-07 18:48:53 -0700

    Initial Commit

---
+++ PlayIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayIt
+//
+//  Created by Nikita Fill on 8/7/15.
+//  Copyright (c) 2015 Speedy Unicorn Studio Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
